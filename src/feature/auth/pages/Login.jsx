7714d1ce46a7ff6d1d4eb6959577b93d22f726fe
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-30 00:05:41 +0530

    Add navigation to home after login and update registration flow to navigate to login

--- src/feature/auth/pages/Login.jsx
+++ src/feature/auth/pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         await handleLogin(email, password)
+        navigate('/')
     }
 
     if (loading) {
--- src/feature/auth/pages/Register.jsx
+++ src/feature/auth/pages/Register.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import {useNavigate , Link} from 'react-router'
+import { useNavigate, Link } from 'react-router'
 
 const Register = () => {
-    const handleSubmit = (e) => {
+    const navigate = useNavigate()
+    const { loading, handleRegister } = useAuth()
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        await handleRegister(username, email, password)
+        navigate('/login')
     }
 
     return (
@@ -13,22 +20,46 @@ const Register = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <label htmlFor="name">Username</label>
-                        <input type="text" id="name" name="name" placeholder="Enter your name" required />
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Enter your name"
+                            required
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <button className="button primary-button" type="submit">Register</button>
                 </form>
                 <p>Already have an account? <Link to={"/login"}>Login</Link></p>
             </div>
         </main>
-  )
+    )
 }
 
 export default Register
