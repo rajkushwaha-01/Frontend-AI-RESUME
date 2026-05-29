c34cb18007a4a675589725aba165ba6945dbc474
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-30 00:49:06 +0530

    Refactor user fetching logic: move useEffect for getMe to useAuth hook and remove from AuthProvider

--- src/feature/auth/hooks/useAuth.js
+++ src/feature/auth/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext , useEffect} from 'react'
 import { AuthContext } from '../auth.context'
 import { login, register, logout, getMe } from '../services/auth.api'
 
@@ -53,6 +53,23 @@ export const useAuth = () => {
             setLoading(false)
         }
     }
+
+
+    useEffect(() => {
+        const getandSetUser = async () => {
+            try {
+                const data = await getMe()
+                setUser(data?.user ?? null)
+            } catch (err) {
+                console.error(err)
+                setUser(null)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getandSetUser()
+    }, [])
 
     return { user, loading, handleLogin, handleRegister, handleLogout, handleGetMe }
 }
