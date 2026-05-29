07782f69e17896b482201ebe9cb298ed9bd138b0
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-29 20:42:31 +0530

    Implement login and registration forms with styling

--- src/feature/auth/pages/Login.jsx
+++ src/feature/auth/pages/Login.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import '../auth.form.scss'
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+    }
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                    </div>
+                    <button className="button primary-button" type="submit">Login</button>
+                </form>
+            </div>
+        </main>
+    )
 }
 
 export default Login
--- src/feature/auth/pages/Register.jsx
+++ src/feature/auth/pages/Register.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="name">Username</label>
+                        <input type="text" id="name" name="name" placeholder="Enter your name" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                    </div>
+                    <button className="button primary-button" type="submit">Login</button>
+                </form>
+            </div>
+        </main>
   )
 }
 
