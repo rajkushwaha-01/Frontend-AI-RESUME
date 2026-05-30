9a8032dcff309e095d9ffbb24573fded88a727b8
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-30 23:15:53 +0530

    URL

--- src/feature/auth/services/auth.api.js
+++ src/feature/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://ai-resume-xvlh.onrender.com/api/auth",
     withCredentials: true
 })
 
